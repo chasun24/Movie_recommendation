4c802a8704ba1c88204f43629a0b9237c0e3ed54
cyan <2818203745@qq.com>
2021-04-13 16:17:44 +0800

    calculate precision and recall on one user

--- CF/original.py
+++ CF/original.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 import math
 import time
+import heapq
+
 
 class userCF:
     global std,userNumMax,movieNumMax,testRate
@@ -15,9 +17,10 @@ class userCF:
 
         ratingsData=self.dataLoad(rate_file)
         trainSet,testSet=self.dataSplit(ratingsData)
-        userSimilarity=self.userSimFunc(trainSet)
-        
-    
+        userSimilarity,userRowModvieMatrix=self.userSimFunc(trainSet)
+        finalMovieRecList=self.recommend(1,userSimilarity,userRowModvieMatrix)
+        precs=self.precision(testSet,finalMovieRecList,1)
+        reCall=self.reCall(userRowModvieMatrix,finalMovieRecList,1)
     def dataLoad(self,rate_file):
         ###数据载入###
 
@@ -37,7 +40,7 @@ class userCF:
         for line in open(rate_file):
             userid,itemid,record,idcode = line.split(",")
             # userSet.add(userid)
-            ratingsData.append((int(userid),int(itemid),float(record)-std))     #data列表 ((user,movie,record),(...)...)
+            ratingsData.append((int(userid),int(itemid),float(record)-std))     #data列表 [(user,movie,record),(...)...]
         # timend=time.time()
         # print("10w数据读取时间:{:.3f}".format(timend-timest))
         return ratingsData
@@ -60,9 +63,10 @@ class userCF:
     ###用户相似度矩阵###
     def userSimFunc(self,trainSet):
         # timest=time.time()
-        if os.path.exists("./data/userSimilarity.npy"):
-            print("用户相似度从文件加载 ...")
+        if os.path.exists("./data/userSimilarity.npy") and os.path.exists("./data/userRowModvieMatrix.npy") :
+            print("从文件加载 ...")
             userSimilarity = np.load("./data/userSimilarity.npy")
+            userRowModvieMatrix=np.load("./data/userRowModvieMatrix.npy")
         else:
             print("开始计算用户相似度 ...")
             userRowModvieMatrix = np.full((userNumMax+1, movieNumMax+1), 0, dtype=float)  #生产usersNum*moviesNum大小的矩阵，默认值为std
@@ -78,11 +82,61 @@ class userCF:
             for i in range(1,userNumMax+1):     #range 左闭右开
                 for j in range(i+1,userNumMax+1):
                     userSimilarity[i][j]=userRowInner[i][j] /(math.sqrt(userRowInner[i][i])*math.sqrt(userRowInner[j][j]))
-            print("保存计算用户相似度文件 ...")
+                    userSimilarity[j][i]=userSimilarity[i][j]
+            print("保存文件 ...")
             np.save("./data/userSimilarity.npy",userSimilarity)
-        return userSimilarity
+            np.save("./data/userRowModvieMatrix.npy",userRowModvieMatrix)
+        return userSimilarity,userRowModvieMatrix
         # timend=time.time()
         # print("计算相似度时间矩阵:{:.3f}".format(timend-timest))
+    def recommend(self,userId,userSimilarity,userRowModvieMatrix,userRfnsNum=10,movieNum=40):
+        userIdFriends=userSimilarity[userId]    # userId与所有用户的相似度向量(1*userNumMax)
+        friendSimilarList=[]                    # 列表，存储[userid,similar]列表，即[[userid,similar],[..]...]
+        for friendId in range(1,userNumMax+1):
+            friendSimilarList.append([friendId,userIdFriends[friendId]])
+        topFriendList=sorted(friendSimilarList, key=lambda dic: dic[1],reverse=True)[0:userRfnsNum]
+        # 按silimar关键字从大到小排序的friendSimilarList，的前userRfnsNum个
+        # lambda 表达式，以列表的列表中第二个为关键字排序
+        allMovList=np.full(movieNumMax+1, 0, dtype=float)
+        for fid in range(0,userRfnsNum):
+            uid=topFriendList[fid][0]
+            sim=topFriendList[fid][1]
+            for mid in range(1,movieNumMax+1):
+                rcd=userRowModvieMatrix[uid][mid]
+                allMovList[mid]+=rcd*sim
+        #所有电影的推荐指数已计算完成
+        allMovieRecList=[]
+        for mid in range(1,movieNumMax+1):
+            allMovieRecList.append([mid,allMovList[mid]])
+        finalMovieRecList=sorted(allMovieRecList, key=lambda dic: dic[1],reverse=True)[0:movieNum]
+        return finalMovieRecList
+        # for mov in finalMovieRecList:
+        #     print("mid:{: >10},rec:{: >30}".format(mov[0],mov[1]))
+    
+    def precision(self,testSet,finalMovieRecList,userId):
+        '''准确率=测试集与推荐中重合的电影/所有推荐的电影
+        '''
+        size=len(finalMovieRecList)
+        testMatrix = np.full(movieNumMax+1, 0, dtype=float)
+        for dataitem in testSet:
+            if dataitem[0]!=userId:
+                continue
+            testMatrix[ dataitem[1] ]=dataitem[2]
+        cnt=0
+        for m in finalMovieRecList:
+            if testMatrix[m[0]]!=0:
+                cnt+=1
+        print("cnt={: >10},precision={: >25}%".format(cnt,cnt/size*100))
+        return cnt/size
+    def reCall(self,userRowModvieMatrix,finalMovieRecList,userId):
+        size=len(finalMovieRecList)
+        cnt=0
+        for m in finalMovieRecList:
+            if userRowModvieMatrix[userId][m[0]]!=0:
+                cnt+=1
+        print("cnt={: >10},reCallrate={: >25}%".format(cnt,cnt/size*100))
+        return cnt/size
+        
 
 
 if __name__=="__main__":
